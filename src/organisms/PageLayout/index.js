755dce4d0ec9c5d68f46aef02a57202a2020ab50
shukii <jbenson@and.digital>
2019-06-22 19:16:09 +0100

    scroll to top on route change' .

--- src/organisms/PageLayout/index.js
+++ src/organisms/PageLayout/index.js
@@ -2,12 +2,21 @@ import React from "react";
 import NavigationBar from "../../molecules/NavigationBar";
 import Footer from "../../organisms/Footer";
 
-const PageLayout = ({ children }) => (
-  <div className="page-layout">
-    <NavigationBar scrollOffset={510} />
-    <div className="page-content">{children}</div>
-    <Footer />
-  </div>
-);
+class PageLayout extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div className="page-layout">
+        <NavigationBar scrollOffset={510} />
+        <div className="page-content">{children}</div>
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default PageLayout;
