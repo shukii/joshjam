d2cd1cf741734d9cdaef43c343e29b9dbd84ff82
shukii <jbenson@and.digital>
2019-06-23 12:33:45 +0100

    page not found

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import {Header} from 'semantic-ui-react';
-import PageLayout from '../../organisms/PageLayout';
+import React from "react";
+import PageLayout from "../../organisms/PageLayout";
+import ImageHeader from "../../organisms/ImageHeader";
+import { Header } from "semantic-ui-react";
+import "./_styles.scss";
 
-const NotFound = () => 
-<PageLayout>
-  <Header>Not Found</Header>
-</PageLayout>
+const NotFound = () => (
+  <PageLayout>
+    <ImageHeader name="Page not found" page="not-found" />
+    <div className="filler">
+      <Header>Sorry, we couldn't find the page you are looking for</Header>
+    </div>
+  </PageLayout>
+);
 
 export default NotFound;
